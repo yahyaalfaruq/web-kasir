574f53ac7b487ae33b6a7113071a103b3d913ec4
jhon <165111230+yahyaalfaruq@users.noreply.github.com>
2025-01-29 08:36:04 +0700

    add pages

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <iframe
+        src="/index.html"
+        style={{ width: "100vw", height: "100vh", border: "none" }}
+      />
+    );
+  }
+  

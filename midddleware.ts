5cb941e8bb78ab6cd1447737ecfa9da385e5464e
jhon <165111230+yahyaalfaruq@users.noreply.github.com>
2025-01-29 09:18:07 +0700

    update index

--- midddleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname === "/") {
-    return NextResponse.rewrite(new URL("/index.html", req.url));
-  }
-  return NextResponse.next();
-}

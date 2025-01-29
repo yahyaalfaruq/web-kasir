43564cad26ed3ce24378f2e6a49f78f767f90934
jhon <165111230+yahyaalfaruq@users.noreply.github.com>
2025-01-29 08:13:24 +0700

    create-app-kasir

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/public/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;

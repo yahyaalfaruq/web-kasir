41277621e50e8b07a739ef3e263766df344b41c0
jhon <165111230+yahyaalfaruq@users.noreply.github.com>
2025-01-29 08:19:56 +0700

    update next

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   async rewrites() {
     return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
       {
         source: '/:path*',
         destination: '/public/:path*',

9a69503aca2469dba85369355428a9c293051854
jhon <165111230+yahyaalfaruq@users.noreply.github.com>
2025-01-29 08:27:26 +0700

    create pages

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,6 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-      },
-      {
-        source: '/:path*',
-        destination: '/public/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
